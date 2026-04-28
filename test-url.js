e1f9de113a4354f69e2b62105783a41055bf24cd
danielvaleweb <139024148+danielvaleweb@users.noreply.github.com>
2026-04-27 23:05:11 -0300

    feat: Update build process and remove test files
    Refactors the build process to output ESM format for the server. Removes unused test files to clean up the project.

--- test-url.js
+++
@@ -1,8 +0,0 @@
-async function run() {
-  const url = `https://www.youtube.com/@igrejaprofetizandoavida`;
-  const res = await fetch(url, { headers: {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-  }});
-  console.log(res.status);
-}
-run();
